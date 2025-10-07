827dd55be4a2a4d5bc862502bccf921a165d33a3
Raydiation <z5360323@ad.unsw.edu.au>
2025-10-07 16:04:31 +1100

    es

--- week4/q4.s
+++ week4/q4.s
@@ -1,60 +1,68 @@
 	.text
+
 MAP_SIZE = 5
 N_POINTS = 4
+
+# main
+# -> main__for_init:
+# -> main__for_cond:
+# -> main__for_body:
+#....
+
 main:
 main__for_init:
-	li	$t0, 0      #i = $t0
+	li	$t0, 0     #i = $t0
+
 main__for_cond:
 	bge	$t0, N_POINTS, main__for_end
 main__for_body:
-       # int row = my_points[i].row;
-       # i = $t0
-       #todo
+	# int row = my_points[i].row;
 
-       # step 1: my_points[i]
-       la	$t1, my_points
-       mul	$t2, $t0, 8     #multiply offset by array size, which is 8
-       add	$t1, $t1, $t2   # add to base address
+	# access this array
+	la	$t2, my_points
 
-       # step 2: .row
-       addi	$t1, $t1, 0
+	mul	$t3, $t0, 8
+	add	$t3, $t3, $t2
 
-       # step 3: value
-	lw	$t2, ($t1)     # row = $t2
+	# add in the row offset
+	add	$t3, $t3, 0
+	lw	$t2, ($t3)     # row = $t2
 
-	addi	$t1, $t1, 4   #.col
-	lw	$t3, ($t1)     #col = $t3
 
-	# base address + (row * num_cols + cols)
-	la	$t4, topography_grid
+	# col = my_points[i].col
+	lw	$t3, 4($t3)    # col = $t3
 
-	# calc offset
-	mul	$t5, $t2, 5   # row * num_cols
-	add	$t5, $t5, $t3  # + cols
+	# element size * (row * total_cols + col)
 
-	mul	$t5, $t5, 4    # mul by array element size
-	add	$t5, $t4, $t5  #adding to base address
+	mul	$t4, $t2, 5
+	add	$t4, $t4, $t3
+	mul	$t4, $t4, 4     #array element size
 
-	lw	$t5, ($t5)     # $t5 = height
+	# add	$t4, $t4, topography_grid
+	lw	$t4, topography_grid($t4)  #$t4 = height
 
 	li	$v0, 4
 	la	$a0, height_str
 	syscall
 
 	li	$v0, 1
-	move	$a0, $t2
+	move	$a0, $t2  #row
+	syscall
+	
+	li	$v0, 11
+	li	$a0, ','
 	syscall
 
-	move	$a0, $t3
+	li	$v0, 1
+	move	$a0, $t3   #col
 	syscall
 
 	li	$v0, 11
 	li	$a0, '='
 	syscall
 
-
 	li	$v0, 1
-	move	$a0, $t5       #print height
+	move	$a0, $t4   #height
 	syscall
 
 	li	$v0, 11
@@ -69,7 +77,10 @@ main__for_end:
 	li	$v0, 0
 	jr	$ra
 
+
 	.data
+
+# 2D grid representing the height data for an area.
 topography_grid:
 	.word	0, 1, 1, 2, 3
 	.word	1, 1, 2, 3, 4
@@ -77,6 +88,8 @@ topography_grid:
 	.word	3, 3, 4, 5, 6
 	.word	3, 4, 5, 6, 7
 
+# Points of interest to print heights for, as a 1D array of point2D_t structs.
+# Note the memory layout of this array: each element requires 8 bytes, not 4.
 my_points:
 	.word	1, 2
 	.word	2, 3
